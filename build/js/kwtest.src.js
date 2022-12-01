27dc792dc283917325041c93f10aee98fc446d1b
jpmaroco <jpmaroco@gmail.com>
2022-12-01 22:00:04 +0000

    updates to KW with post hoc
    Cosmetic stuff

--- build/js/kwtest.src.js
+++ build/js/kwtest.src.js
@@ -3,7 +3,7 @@
 
 'use strict';
 
-const options = [{"name":"data","type":"Data"},{"name":"dep","title":"Variável Dependente","type":"Variable","required":true,"suggested":["continuous","ordinal"],"permitted":["numeric"],"description":{"ui":"A variável dependente. Tem que ser pelo menos ordinal.\n","R":"A variável dependente. Tem que ser pelo menos ordinal.\n"}},{"name":"group","title":"Variável de Agrupamento","type":"Variable","required":true,"suggested":["nominal","ordinal"],"description":{"ui":"A variável independente. Nominal ou ordinal.\n","R":"A variável independente. Nominal ou ordinal..\n"}},{"name":"posts","title":"Testes Post-hoc","type":"List","options":["Nenhum","Dunn","Iman-Conover","DSCF"],"default":"Nenhum","description":{"ui":"Faz testes post-hoc.\n","R":"Teste de Dunn sem correção e com correção a escolher (Bonferroni-Dunn é a mais usual)\n"}},{"name":"method","title":"Correção p-value de Dunn","type":"List","options":[{"title":"Bonferroni","name":"bonferroni"},{"title":"Sidak","name":"sidak"},{"title":"Holm","name":"holm"}]}];
+const options = [{"name":"data","type":"Data"},{"name":"dep","title":"Variável Dependente","type":"Variable","required":true,"suggested":["continuous","ordinal"],"permitted":["numeric"],"description":{"ui":"A variável dependente. Tem que ser pelo menos ordinal.\n","R":"A variável dependente. Tem que ser pelo menos ordinal.\n"}},{"name":"group","title":"Variável de Agrupamento","type":"Variable","required":true,"suggested":["nominal","ordinal"],"description":{"ui":"A variável independente. Nominal ou ordinal.\n","R":"A variável independente. Nominal ou ordinal..\n"}},{"name":"posts","title":"Testes Post-hoc","type":"List","options":["Nenhum","Dunn","Iman-Conover","DSCF"],"default":"Nenhum","description":{"ui":"Faz testes post-hoc.\n","R":"Teste de Dunn sem correção e com correção a escolher (Bonferroni-Dunn é a mais usual)\n"}},{"name":"method","title":"Correção p-value de Dunn/Iman","type":"List","options":[{"title":"Bonferroni","name":"bonferroni"},{"title":"Sidak","name":"sidak"},{"title":"Holm","name":"holm"}]}];
 
 const view = function() {
     
@@ -63,34 +63,87 @@ view.layout = ui.extend({
 			]
 		},
 		{
-			type: DefaultControls.Label,
-			typeName: 'Label',
+			type: DefaultControls.CollapseBox,
+			typeName: 'CollapseBox',
 			label: "Testes Post-Hoc",
-			controls: [
-
-			]
-		},
-		{
-			type: DefaultControls.LayoutBox,
-			typeName: 'LayoutBox',
-			margin: "large",
+			collapsed: true,
+			stretchFactor: 1,
 			controls: [
 				{
-					type: DefaultControls.ComboBox,
-					typeName: 'ComboBox',
-					name: "posts"
-				}
-			]
-		},
-		{
-			type: DefaultControls.LayoutBox,
-			typeName: 'LayoutBox',
-			margin: "large",
-			controls: [
+					type: DefaultControls.LayoutBox,
+					typeName: 'LayoutBox',
+					margin: "large",
+					stretchFactor: 1,
+					controls: [
+						{
+							type: DefaultControls.LayoutBox,
+							typeName: 'LayoutBox',
+							cell: {"column":0,"row":0},
+							stretchFactor: 1,
+							controls: [
+								{
+									type: DefaultControls.Label,
+									typeName: 'Label',
+									label: "Testes Post-Hoc",
+									controls: [
+										{
+											type: DefaultControls.RadioButton,
+											typeName: 'RadioButton',
+											name: "postsNenhum",
+											optionName: "posts",
+											optionPart: "Nenhum",
+											label: "Nenhum"
+										},
+										{
+											type: DefaultControls.RadioButton,
+											typeName: 'RadioButton',
+											name: "postsDunn",
+											optionName: "posts",
+											optionPart: "Dunn",
+											label: "Dunn"
+										},
+										{
+											type: DefaultControls.RadioButton,
+											typeName: 'RadioButton',
+											name: "postsImanConover",
+											optionName: "posts",
+											optionPart: "Iman-Conover",
+											label: "Iman-Conover"
+										},
+										{
+											type: DefaultControls.RadioButton,
+											typeName: 'RadioButton',
+											name: "postsDSCF",
+											optionName: "posts",
+											optionPart: "DSCF",
+											label: "DSCF"
+										}
+									]
+								}
+							]
+						}
+					]
+				},
 				{
-					type: DefaultControls.ComboBox,
-					typeName: 'ComboBox',
-					name: "method"
+					type: DefaultControls.LayoutBox,
+					typeName: 'LayoutBox',
+					margin: "large",
+					stretchFactor: 1,
+					controls: [
+						{
+							type: DefaultControls.LayoutBox,
+							typeName: 'LayoutBox',
+							cell: {"column":0,"row":0},
+							stretchFactor: 1,
+							controls: [
+								{
+									type: DefaultControls.ComboBox,
+									typeName: 'ComboBox',
+									name: "method"
+								}
+							]
+						}
+					]
 				}
 			]
 		}
